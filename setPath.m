47b0166cc895872799c1734a7da22af378830cb2
babakahari87 <47870670+babakahari87@users.noreply.github.com>
2019-05-30 21:08:29 +0430

    Add files via upload

---
+++ setPath.m
@@ -0,0 +1,17 @@
+% Adds directories to MATLAB path
+
+% Paths for the linear method
+addpath(fullfile(pwd, 'Linear'));
+addpath(fullfile(pwd, 'Util'));
+addpath(fullfile(pwd, 'matlabPyrTools'));
+addpath(fullfile(pwd, 'matlabPyrTools', 'MEX'));
+
+% Paths for the phase-based method
+addpath(fullfile(pwd, 'PhaseBased'));
+addpath(fullfile(pwd, 'pyrToolsExt'));
+addpath(fullfile(pwd, 'Filters'));
+addpath(fullfile(pwd, 'data'));
+addpath(fullfile(pwd, 'data\parts of picture'));
+addpath(fullfile(pwd, 'data\parts of picture\motor'));
+
+addpath('C:\ProgramData\MATLAB\SupportPackages\R2017b\toolbox\vision\supportpackages\visionopencv\example\ORB');
